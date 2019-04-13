d9db2d9ee5d6ca22df2beda9d0ced303c4151d12
Mike3204 <cypher3204@gmail.com>
2019-04-14 01:29:46 +0700

    Connect redux, usage form

--- frontend/src/modules/pages/startPage.tsx
+++ frontend/src/modules/pages/startPage.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField'
+import TextField from '@material-ui/core/TextField';
 
 interface IState {
     inputValue: string
--- frontend/src/modules/shared/components/App.tsx
+++ frontend/src/modules/shared/components/App.tsx
@@ -1,15 +1,51 @@
 import * as React from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 import * as styles from './App.pcss';
 import Main from '../../pages/startPage';
+import AddUsage from '../../pages/AddUsage';
+import ChartPage from '../../pages/chartPage/ChartPage';
+import fillUsageReducer from '../../store/reducers';
 // import StatisticsPage from '../../statistics/components/StatisticsPage';
+
+const store = createStore(fillUsageReducer);
+
+const theme = createMuiTheme();
 
 export default class App extends React.Component {
   public render(): React.ReactNode {
     return (
-      <div className={styles.container}>
-        <Main />
-      </div>
+      <ThemeProvider theme={theme} >
+      <Provider store={store}>
+        
+          <div className={styles.container}>
+            <Router>
+              <ChartPage />
+
+              <Route path="/" exact component={Main} />
+              <Route path="/usage" component={AddUsage} />
+              <Route path="/tariffs" component={Tariffs} />
+            </Router>
+          </div>
+        
+      </Provider>
+      </ThemeProvider>
     );
+  }
+}
+
+class Usage extends React.Component {
+  render() {
+    return <div>usage</div>
+  }
+}
+
+class Tariffs extends React.Component {
+  render() {
+    return <div>tariffs</div>
   }
 }
